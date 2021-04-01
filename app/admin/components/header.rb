8f4483824746a77144a98124db3b5e2414087c71
Antoine Goeuriot <antoine.goeuriot@gmail.com>
2021-04-01 17:51:43 +0200

    trying another way for rb files

--- app/admin/components/header.rb
+++ app/admin/components/header.rb
@@ -1,29 +1,33 @@
-ActiveAdmin::Views::Header.class_eval do
-  def build(namespace, menu)
-    super(id: 'header')
+module ActiveAdmin
+  module Views
+    class Header < Component
+      def build(namespace, menu)
+        super(id: 'header')
 
-    @namespace = namespace
-    @menu = menu
-    @utility_menu = @namespace.fetch_menu(:utility_navigation)
+        @namespace = namespace
+        @menu = menu
+        @utility_menu = @namespace.fetch_menu(:utility_navigation)
 
-    build_site_header
-  end
+        build_site_header
+      end
 
-  def build_site_header
-    div class: 'w-100' do
-      div class: 'd-flex justify-content-between px-4 py-2' do
-        div do
-          site_title @namespace
+      def build_site_header
+        div class: 'w-100' do
+          div class: 'd-flex justify-content-between px-4 py-2' do
+            div do
+              site_title @namespace
+            end
+
+            insert_tag view_factory.global_navigation, @utility_menu, id: 'utility_nav', class: 'header-item tabs'
+          end
+
+          div class: 'border-bottom-white-50 pt-1 mx-4' do
+          end
+
+          div class: 'p-4' do
+            insert_tag view_factory.global_navigation, @menu, class: 'header-item tabs'
+          end
         end
-
-        insert_tag view_factory.global_navigation, @utility_menu, id: 'utility_nav', class: 'header-item tabs'
-      end
-
-      div class: 'border-bottom-white-50 pt-1 mx-4' do
-      end
-
-      div class: 'p-4' do
-        insert_tag view_factory.global_navigation, @menu, class: 'header-item tabs'
       end
     end
   end
